6cb2ec4c8f538d613828b11dc7bfa9460caf8d8a
SULAVSHRESTHA08 <141043235+SULAVSHRESTHA08@users.noreply.github.com>
2026-07-06 09:11:23 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+      pairs  = {} #Mapping the charcater count to list of Anagrams 
+      for s in strs :
+        count = Counter(s) # Create a count of characters for the current string
+        # Convert the Counter to a tuple of sorted items to make it hashable 
+        key = tuple(sorted(count.items())) # signature for the anagrams 
+
+        if key not in pairs:
+          pairs[key] = []
+        pairs[key].append(s)
+      return list(pairs.values())        
